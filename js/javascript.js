113d6b541f017e1eda1a01e6029087bfc9497460
bmartin86 <iMuy11pMpk>
2024-01-29 23:44:58 +0100

    sortiranje čišćenje i izmjena index.html

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,5 @@
-  //load header
+//load header
+if (document.getElementById("header")) {
   function loadHeader() {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function () {
@@ -8,120 +9,135 @@
     xhttp.send();
   }
   loadHeader();
+}
 
 
-  //loadProductCard
-  if (document.getElementsByClassName("productCard")) {
-    function loadProductCard() {
-      const xhttp = new XMLHttpRequest();
-      xhttp.onload = function () {
-        // Get all elements with the class 'productCard'
-        const productCards = document.getElementsByClassName("productCard");
-    
-        // Iterate through the elements and update their content
-        for (let i = 0; i < productCards.length; i++) {
-          productCards[i].innerHTML = this.responseText;
-        }
-      };
-      xhttp.open("GET", "../partials/productCard.html");
-      xhttp.send();
-    }
-    loadProductCard();
-  }
-
-
-  //loadBlogCard
-  if (document.getElementsByClassName("blogCard")) {
-    function loadBlogCard() {
-      const xhttp = new XMLHttpRequest();
-      xhttp.onload = function () {
-       
-        // Get all elements with the class 'blogCard'
-        const blogCards = document.getElementsByClassName("blogCard");
-        // Iterate through the elements and update their content
-        for (let i = 0; i < blogCards.length; i++) {
-          
-          blogCards[i].innerHTML = this.responseText;
-        }
-      };
-      xhttp.open("GET", "../partials/blogCard.html");
-      xhttp.send();
-    }
-  }
-
-
-  //loadBlogCardLandscape
-  if ( document.getElementsByClassName("blogCardLandscape")) {
-    function loadBlogCardLandscape() {
-      const xhttp = new XMLHttpRequest();
-      xhttp.onload = function () {
-        // Get all elements with the class 'blogCardLandscape'
-        const blogCardLandscape = document.getElementsByClassName("blogCardLandscape");
-    
-        // Iterate through the elements and update their content
-        for (let i = 0; i < blogCardLandscape.length; i++) {
-          blogCardLandscape[i].innerHTML = this.responseText;
-        }
-      };
-      xhttp.open("GET", "../partials/blogCardLandscape.html");
-      xhttp.send();
-    }
-    loadBlogCardLandscape();
-  }
-
-
-
-  //load productListCard
-  if (document.getElementById("productListCard")) {
-    function loadProductListCard() {
-      const xhttp = new XMLHttpRequest();
-      xhttp.onload = function () {
-        document.getElementById("productListCard").innerHTML = this.responseText;
-      }
-      xhttp.open ("GET", "../partials/productListCard.html");
-      xhttp.send();
-    }
-    loadProductListCard();
-  }
-
-  
-
-  //load asideProducts
-  if(document.getElementById("asideProducts")) {
-    function loadAsideProducts() {
-      const xhttp = new XMLHttpRequest();
-      xhttp.onload = function () {
-        document.getElementById("asideProducts").innerHTML = this.responseText;
-      }
-      xhttp.open ("GET", "../partials/asideProducts.html");
-      xhttp.send();
-    }
-    loadAsideProducts();  
-  }
- 
-
-  //load asideBlog
-  if(document.getElementById("asideBlog")) {
-    function loadAsideBlog() {
-      const xhttp = new XMLHttpRequest();
-      xhttp.onload = function () {
-        document.getElementById("asideBlog").innerHTML = this.responseText;
-        loadBlogCard();
-      }
-      xhttp.open ("GET", "../partials/asideBlog.html");
-      xhttp.send();
-    }
-    loadAsideBlog();
-  }
-
-
-  //load footer
-  function loadFooter() {
+//loadProductCard
+if (document.getElementsByClassName("productCard")) {
+  function loadProductCard() {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function () {
-      document.getElementById("footer").innerHTML = this.responseText;
-    }
-    xhttp.open ("GET", "../partials/footer.html");
+      // Get all elements with the class 'productCard'
+      const productCards = document.getElementsByClassName("productCard");
+  
+      // Iterate through the elements and update their content
+      for (let i = 0; i < productCards.length; i++) {
+        productCards[i].innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "../partials/productCard.html");
     xhttp.send();
   }
-  loadFooter();
+  loadProductCard();
+}
+
+
+//load productListCard
+if (document.getElementById("productListCard")) {
+  function loadProductListCard() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+      document.getElementById("productListCard").innerHTML = this.responseText;
+      loadProductCard();
+    }
+    xhttp.open ("GET", "../partials/productListCard.html");
+    xhttp.send();
+  }
+  loadProductListCard();
+}
+
+
+//load productCategories
+if (document.getElementById("productCategories")) {
+  function loadProductCategories() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+      document.getElementById("productCategories").innerHTML = this.responseText;
+      loadProductListCard();
+    }
+    xhttp.open ("GET", "../partials/productCategories.html");
+    xhttp.send();
+  }
+  loadProductCategories();
+}
+
+
+//load asideProducts
+if(document.getElementById("asideProducts")) {
+  function loadAsideProducts() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+      document.getElementById("asideProducts").innerHTML = this.responseText;
+    }
+    xhttp.open ("GET", "../partials/asideProducts.html");
+    xhttp.send();
+  }
+  loadAsideProducts();  
+}
+
+  
+//loadBlogCard
+if (document.getElementsByClassName("blogCard")) {
+  function loadBlogCard() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+      
+      // Get all elements with the class 'blogCard'
+      const blogCards = document.getElementsByClassName("blogCard");
+      // Iterate through the elements and update their content
+      for (let i = 0; i < blogCards.length; i++) {
+        
+        blogCards[i].innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "../partials/blogCard.html");
+    xhttp.send();
+  }
+}  
+
+
+//loadBlogCardLandscape
+if ( document.getElementsByClassName("blogCardLandscape")) {
+  function loadBlogCardLandscape() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+      // Get all elements with the class 'blogCardLandscape'
+      const blogCardLandscape = document.getElementsByClassName("blogCardLandscape");
+  
+      // Iterate through the elements and update their content
+      for (let i = 0; i < blogCardLandscape.length; i++) {
+        blogCardLandscape[i].innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "../partials/blogCardLandscape.html");
+    xhttp.send();
+  }
+  loadBlogCardLandscape();
+}
+ 
+
+//load asideBlog
+if(document.getElementById("asideBlog")) {
+  function loadAsideBlog() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+      document.getElementById("asideBlog").innerHTML = this.responseText;
+      loadBlogCard();
+    }
+    xhttp.open ("GET", "../partials/asideBlog.html");
+    xhttp.send();
+  }
+  loadAsideBlog();
+}
+
+
+//load footer
+function loadFooter() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function () {
+    document.getElementById("footer").innerHTML = this.responseText;
+  }
+  xhttp.open ("GET", "../partials/footer.html");
+  xhttp.send();
+}
+loadFooter();
--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
+window.onload = function() {
+  var video = document.getElementById("hero-video");
+  video.play();
+};
+
+
 window.onscroll = function () {
   scroll();
 };
- function scroll() {
-   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    document.getElementById("wrapper").style.top = "0";
-   } else {
-    document.getElementById("wrapper").style.top = "-135px";
-   }
- };
- window.onload = function() {
-   var video = document.getElementById("hero-video");
-   video.play();
- };
+
+function scroll() {
+  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+  if (scrollTop > 60 && scrollTop <= 1380) {
+   document.getElementById("wrapper").style.top = "0";
+  } else {
+   document.getElementById("wrapper").style.top = "-135px";
+  }
+};
